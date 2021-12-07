c62d3bcd6a0a024231c1768c5a67a88bf7860c50
YingjiaZ <szhangyingjia@gmail.com>
2021-12-06 19:12:31 -0500

    added drop-down and animation

--- raster.js
+++ raster.js
@@ -6,8 +6,7 @@ const margin = { top: 10, right: 10, bottom: 70, left:60};
 const chartWidth = width - margin.left - margin.right;
 const chartHeight = height - margin.top - margin.bottom;
     
-// Add a canvas element
-const canvas = container.append("canvas")
+const svgA = container.append("svg")
                         .attr("width",chartWidth)
                         .attr("height",chartHeight)
                         .style("position","absolute")
@@ -25,16 +24,16 @@ const yAxisArea = svg.append("g").attr("transform","translate("+(margin.left-10)
 const xAxisArea = svg.append("g").attr("transform","translate("+margin.left+","+(chartHeight+2+margin.top)+")");
 const label = svg.append("text").attr("x", margin.left+10).attr("y", margin.top+10);
 
-const svgLegend = d3.select("raster-legend")
-                 .append("svg")
-                 .attr("width",width)
-                 .attr("height", 70)
-svgLegend.append("rect")
-        .attr("x", 10)
-        .attr("y", 10)
-        .attr("width", 20)
-        .attr("height", 15)
-        .style("fill", d3.interpolateYlGnBu())
+// const svgLegend = d3.select("raster-legend")
+//                  .append("svg")
+//                  .attr("width",width)
+//                  .attr("height", 70)
+// svgLegend.append("rect")
+//         .attr("x", 10)
+//         .attr("y", 10)
+//         .attr("width", 20)
+//         .attr("height", 15)
+//         .style("fill", d3.interpolateYlGnBu())
 
 
 const interactiveArea = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
@@ -54,7 +53,7 @@ const drawPlot = async () => {
     let categories = new Set(data.map(d => d.Major_category))
     console.log(categories)
     const xScale = d3.scalePoint().domain(categories).range([10, chartWidth-10]);
-    const yScale = d3.scaleLinear().domain(d3.extent(data, d => d.Median)).range([chartHeight-10, 10]);
+    let yScale = d3.scaleLinear().domain(d3.extent(data, d => d.Median)).range([chartHeight-10, 10]);
     const cScale = d3.scaleLinear().domain(d3.extent(data, d => d.Unemployment_rate)).range([0, 1]);
     const colorScale = d3.scaleSequential( d => d3.interpolateYlGnBu(cScale(d)) );
 
@@ -74,69 +73,140 @@ const drawPlot = async () => {
              .style("font-size", "11px")
              .attr("transform", "rotate(-18)");
 
-    const context = canvas.node().getContext('2d');
-    context.clearRect(0, 0, width, height);
-    context.globalAlpha = 0.6;
     data.forEach( (d,i) => {
-    
-        let x = xScale(d.Major_category);
-        let y = yScale(d.Median);
-        let rad = 8;
-        let color = colorScale(d.Unemployment_rate);
 
-        context.fillStyle = color;
-        context.beginPath();
-        context.arc(x, y, rad, 0, 2*Math.PI);
-        context.fill();
-
+        svgA.append("circle")
+           .attr("cx", xScale(d.Major_category))
+           .attr("cy", yScale(d.Median))
+           .style("fill", colorScale(d.Unemployment_rate))
+           .attr("r",8)
+           .style("opacity",0.6)
     });
 
-    const delaunay = d3.Delaunay.from(data, d => xScale(d.Major_category), d => yScale(d.Median));
+      const delaunay = d3.Delaunay.from(data, d => xScale(d.Major_category), d => yScale(d.Median));
 
-    let currentTarget = -1;
-    interactiveArea.on("mousemove", function(event) {
+      let currentTarget = -1;
+      interactiveArea.on("mousemove", function(event) {
 
-        let loc = d3.pointer(event);
-        let index;
-        if (currentTarget === -1) {
+          let loc = d3.pointer(event);
+          let index;
+          if (currentTarget === -1) {
 
-          index = delaunay.find(loc[0],loc[1]);   
-        }
-        else {
-          
-          index = delaunay.find(loc[0],loc[1],currentTarget)
-        }
-        if (index !== currentTarget) {
+            index = delaunay.find(loc[0],loc[1]);   
+          }
+          else {
             
-            console.log("mouseover");
-            currentTarget = index;
-            mouseExited();
-            mouseEntered(index);
-         }
-       });
+            index = delaunay.find(loc[0],loc[1],currentTarget)
+          }
+          if (index !== currentTarget) {
+              
+              console.log("mouseover");
+              currentTarget = index;
+              mouseExited();
+              mouseEntered(index);
+          }
+        });
 
-       interactiveArea.on("mouseout", () => {
-         currentTarget = -1;
-         mouseExited(); 
-       })
- 
- 
-       function mouseEntered(i) {
-            let d = data[i];
-            interactiveArea.append("circle")
-                        .attr("id","highlight")
-                        .attr("cx",xScale(d.Major_category))
-                        .attr("cy",yScale(d.Median))
-                        .attr("r",10)
-                        .attr("stroke","black")
-                        .attr("fill","none");
-            label.text(d.Major);
-                       
-       }
-       function mouseExited() {
-         d3.select("#highlight").remove();
-         label.text("");
-       }
+        interactiveArea.on("mouseout", () => {
+          currentTarget = -1;
+          mouseExited(); 
+        })
+  
+  
+        function mouseEntered(i) {
+              let d = data[i];
+              interactiveArea.append("circle")
+                          .attr("id","highlight")
+                          .attr("cx",xScale(d.Major_category))
+                          .attr("cy",yScale(d.Median))
+                          .attr("r",10)
+                          .attr("stroke","black")
+                          .attr("fill","none");
+              label.text(d.Major);
+                        
+        }
+        function mouseExited() {
+          d3.select("#highlight").remove();
+          label.text("");
+        }
+        
+
+        var incomeSelect = d3.select("#raster-dropdown");
+        var selectedValue = "Median";
+        let dropValues = ["Median", "P25th", "P75th"];
+        let dataTypes = ["Median Income", "25th percentile of earnings", "75th percentile of earnings"];
+
+        dataTypes.forEach((d, i) => {
+          incomeSelect.append("option")
+                      .attr("value", dropValues[i])
+                      .property("selected", () => { return (d === "Median Income") })
+                      .text(d)
+        });
+
+        incomeSelect.on("change", updateChart)
+
+        function updateChart() {
+          selectedValue = d3.select(this).property("value");
+          yScale = d3.scaleLinear().domain(d3.extent(data, d => d[selectedValue])).range([chartHeight-10, 10]);
+          yAxis = d3.axisLeft(yScale); 
+          yAxisArea.select(".y.axis").call(yAxis); 
+
+          svgA.selectAll("circle")
+                .data(data)
+                .transition()
+                .delay(function(d,i){return(i*3)})
+                .duration(2000)
+                .attr("cx", d => xScale(d.Major_category))
+                .attr("cy", d => yScale(d[selectedValue]))
+
+          const delaunay = d3.Delaunay.from(data, d => xScale(d.Major_category), d => yScale(d[selectedValue]));
+
+          let currentTarget = -1;
+          interactiveArea.on("mousemove", function(event) {
+
+          let loc = d3.pointer(event);
+          let index;
+          if (currentTarget === -1) {
+
+            index = delaunay.find(loc[0],loc[1]);   
+          }
+          else {
+            
+            index = delaunay.find(loc[0],loc[1],currentTarget)
+          }
+          if (index !== currentTarget) {
+              
+              // console.log("mouseover");
+              currentTarget = index;
+              mouseExited();
+              mouseEntered(index);
+          }
+        });
+
+        interactiveArea.on("mouseout", () => {
+          currentTarget = -1;
+          mouseExited(); 
+        })
+  
+  
+        function mouseEntered(i) {
+              let d = data[i];
+              interactiveArea.append("circle")
+                          .attr("id","highlight")
+                          .attr("cx",xScale(d.Major_category))
+                          .attr("cy",yScale(d[selectedValue]))
+                          .attr("r",10)
+                          .attr("stroke","black")
+                          .attr("fill","none");
+              label.text(d.Major);
+                        
+        }
+        function mouseExited() {
+          d3.select("#highlight").remove();
+          label.text("");
+        }
+
+      }
 
 }
 drawPlot()
